815b5283fa655927aa7a8a8664378074010cba89
sukumarss <137079093+sukumarss@users.noreply.github.com>
2023-11-19 17:45:44 +0530

    Update CollectingCandies.java
    removed array list

--- training/collecting candies/CollectingCandies.java
+++ training/collecting candies/CollectingCandies.java
@@ -1,40 +1,29 @@
-
-import java.util.Arrays;
 import java.util.Scanner;
 
+public class collecting {
+	 public static void main(String[] args){
+		 Scanner sc = new Scanner(System.in);
+	     int test=sc.nextInt();
+	     for(int i=0;i<test;i++){
+             int N=sc.nextInt();
+	         int[] array=new int[N];
+	         for(int j=0;j<N;j++){
+	             array[j]=sc.nextInt();
+	         }
+	         int sum=array[0];
+	         int a =seconds(array,N,sum);
+	         System.out.println(a);
 
-public class CollectingCandies {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for(int i =0;i<t;i++){
-            int n = sc.nextInt();
-            int[] candies = new int[n];
-            for(int j =0;j<n;j++){
-                candies[j]=sc.nextInt();
-            }
-            long minTime = calculateMinTime(candies, n);
+	     }    
+	 }
+	 static int seconds(int[] array,int N,int sum) {
+		 int sec=0;
+		 for(int k = 0;k<N-1;k++) {
+			 sum+=array[k+1];
+			 sec+=sum;
+			 
+		 }
+		 return sec;
+	 }
 
-            System.out.println(minTime);
-
-        }
-        
-    }
-    private static long calculateMinTime(int[] candies, int n) {
-        Arrays.sort(candies);
-        long totalTime = 0;
-        for (int i = 0; i < n- 1; i++) {
-            int combinedCandies = candies[i] + candies[i + 1];
-            totalTime += combinedCandies;
-
-            candies[i + 1] = combinedCandies;
-
-            Arrays.sort(candies);
-        }
-
-        return totalTime;
-
-
-    }
-    
 }
